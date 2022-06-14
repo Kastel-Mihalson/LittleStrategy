4e46e260b45d042005fc23da0774e46353edbc99
KastelMihalson <constanto147@yandex.ru>
2022-06-14 08:13:04 +0300

    lesson-4. Исправление работы команды stop

--- Assets/LittleStrategy/Scripts/UserControlSystem/UI/Model/CommandCreators/StopCommandCommandCreator.cs
+++ Assets/LittleStrategy/Scripts/UserControlSystem/UI/Model/CommandCreators/StopCommandCommandCreator.cs
@@ -1,9 +1,13 @@
 using System;
+using Zenject;
 
 public class StopCommandCommandCreator : CommandCreatorBase<IStopCommand>
 {
+    [Inject] private AssetsContext _context;
+
     protected override void ClassSpecificCommandCreation(Action<IStopCommand> creationCallback)
     {
+        creationCallback?.Invoke(_context.Inject(new StopCommand()));
     }
 }
 
